3377db6a775d3b78c3284bfc3a2ce89ab7cad91b
Dhenish <dhenish.jivani@vijigi.com>
2024-01-30 10:39:37 +0530

    Add my credentials

--- config.php
+++ config.php
@@ -1,8 +1,7 @@
 <?php
 	
-	$con=mysqli_connect("localhost","jigar","Jigar@1234","vijigi") 
+	$con=mysqli_connect("localhost","root","","vijigi3") 
 	or 
 	die("cannot establish connection with database");
 	
-
 ?>
